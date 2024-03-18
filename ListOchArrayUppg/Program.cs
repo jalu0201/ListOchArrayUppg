b5dd63253bd8436244b3ccfbd77d77ced0b3cc89
Jacob Lundgren <jacob.lundgren@skola.sala.se>
2024-03-18 12:27:03 +0100

    Add project files.

---
+++ ListOchArrayUppg/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ListOchArrayUppg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testTal = { 1, 2, 3,6,8 };
+
+            // Summan
+            Console.WriteLine("Demonstration av Summan:");
+            Console.WriteLine("Summan av 1, 2, 3, 6, 8 är: " + Summan(testTal));
+
+            // Största talet
+            Console.WriteLine();
+            Console.WriteLine("Demonstration av Största talet:");
+            Console.WriteLine("Största talet av 1, 2, 3 är: "); // TODO
+        }
+
+        public static int Summan(int[] listOfNumbers)
+        {
+           int sum = 0;
+            for(int i = 0; i < listOfNumbers.Length; i++)
+            {
+                sum = sum + listOfNumbers[i];
+            }
+
+            return sum;
+        }
+
+    }
+}
