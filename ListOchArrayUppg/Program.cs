205dc0f9323515ca6d439148475162bfa531df80
Jacob Lundgren <jacob.lundgren@skola.sala.se>
2024-03-19 14:27:35 +0100

    Lade till största talet

--- ListOchArrayUppg/Program.cs
+++ ListOchArrayUppg/Program.cs
@@ -5,15 +5,16 @@
         static void Main(string[] args)
         {
             int[] testTal = { 1, 2, 3,6,8 };
+            List<int> testIntList = new List<int>() { 5,7,54,43,3};
 
             // Summan
             Console.WriteLine("Demonstration av Summan:");
-            Console.WriteLine("Summan av 1, 2, 3, 6, 8 är: " + Summan(testTal));
+            Console.WriteLine("Summan av testdatan är: " + Summan(testTal));
 
             // Största talet
             Console.WriteLine();
             Console.WriteLine("Demonstration av Största talet:");
-            Console.WriteLine("Största talet av 1, 2, 3 är: "); // TODO
+            Console.WriteLine("Största talet av testdatan är: " + GreatestNumber(testIntList)); // TODO
         }
 
         public static int Summan(int[] listOfNumbers)
@@ -26,6 +27,20 @@
 
             return sum;
         }
+        public static int GreatestNumber(List<int> numbers)
+        {
+            int största = 0;
+            for (int i = 0; i <  numbers.Count; i++)
+            {if (numbers[i] > största)
+                {
+                    största = numbers[i]; 
+                }
+                
+            }
+            return största; 
+        }
+
+
 
     }
 }
