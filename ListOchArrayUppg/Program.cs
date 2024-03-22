b7f0dda8d3f10d49061ca906c1253967d43e52b6
Jacob Lundgren <jacob.lundgren@skola.sala.se>
2024-03-22 10:35:19 +0100

    Concatenate

--- ListOchArrayUppg/Program.cs
+++ ListOchArrayUppg/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ListOchArrayUppg
+﻿using System;
+
+namespace ListOchArrayUppg
 {
     internal class Program
     {
@@ -26,11 +28,20 @@
             int[] swapTestData = { 8, 4, 6, 9, 2, 7, 3 };
             Console.WriteLine();
             Console.WriteLine("Demonstration av Swap:");
-            Console.WriteLine("Före: element på index 2: " + swapTestData[2]); // 6
-            Console.WriteLine("Före: element på index 5: " + swapTestData[5]); // 7
+            Console.WriteLine("Före: element på index 2: " + swapTestData[2]); 
+            Console.WriteLine("Före: element på index 5: " + swapTestData[5]); 
             Swap(2, 5, swapTestData);
-            Console.WriteLine("Efter: element på index 2: " + swapTestData[2]); // 7
-            Console.WriteLine("Efter: element på index 5: " + swapTestData[5]); // 6
+            Console.WriteLine("Efter: element på index 2: " + swapTestData[2]);
+            Console.WriteLine("Efter: element på index 5: " + swapTestData[5]);
+
+            // Concatenate
+            string[] testPart1 = { "foo", "bar", "baz" };
+            string[] testPart2 = { "apa", "bpa", "cpa" };
+            Console.WriteLine("Demonstration av Concatenate:");
+            string[] concatResult = Concatenate(testPart1, testPart2);
+            Console.WriteLine("Sammansatt array: " + string.Join(",", concatResult));
+
+
 
 
         }
@@ -69,13 +80,29 @@
             return false; 
         }
 
-        // byter plats på element i intarray på index i1 och i2
+     
         static void Swap(int i1, int i2, int[] intarray)
         {
             int swap = intarray[i1];
             intarray[i1] = intarray[i2];
             intarray[i2] = swap;
            
+        }
+
+        public static string[] Concatenate(string[] part1, string[] part2)
+        {
+            string[] result = new string[part1.Length + part2.Length];
+            for(int i = 0; i< part1.Length; i++)
+            {
+                result[i] = part1[i];
+            }         
+            for(int i = 0; i < part2.Length; i++)
+            {
+                result[part1.Length + i] = part2[i];
+            }
+
+
+            return result ;
         }
 
 
